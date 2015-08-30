c9ceaaf2fd2da612e6440a6bd0055b3a1ff5bd4d
James Murphy <james@recumbent.co.uk>
2015-08-30 14:37:29 +0100

    Start with simple create / add functionality

--- New-ToDo.Tests.ps1
+++ New-ToDo.Tests.ps1
@@ -3,7 +3,34 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 
 Describe "New-ToDo" {
-    It "does something useful" {
-        $true | Should Be $false
+    Mock GetToDoFilePath { "TestDrive:\todo.txt" }
+    It "should create file if it doesn't exist" {
+        $TestTask = "This is something that needs to be done"
+        New-Todo $testTask
+
+        $todoFile = GetToDoFilePath
+
+        $todoFile | Should Exist
+    }
+
+    It "should add task to file" {
+        $TestTask = "This is something that needs to be done"
+        New-Todo $testTask
+
+        $todoFile = GetToDoFilePath
+
+        $todoFile | Should Contain $testTask
     }
 }
+
+#New-Todo should fail if no task specified
+
+#New-Todo should accept valid priority
+#New-Todo should reject invalid priority
+#New-Todo should prefix with priority if priority valid
+#New-Todo should add due: if -due parameter with valid date
+
+#Get-ToDo should list incomplete todos with a numeric prefix (need to define order?)
+#Get-ToDo with completed flag should list completed todos with numeric prefix
+
+#Set-todo with done flag should set the specified (by number) todo to done (prefix entry with an "x ")
--- New-ToDo.ps1
+++ New-ToDo.ps1
@@ -1,3 +1,19 @@
-﻿function New-ToDo {
+﻿function GetToDoFilePath
+{
+    "c:\temp\todo.txt"
+}
 
+function New-ToDo {
+    Param(
+    [string] $task
+    )
+
+    $filePath = GetToDoFilePath
+    
+    if (-not (Test-Path $filePath))
+    {
+        New-Item -Path $filePath -ItemType File
+    }
+
+    Add-Content $filePath $task
 }
